1cea58abb27fef62bd8dc28fefc2b87f628bd6d5
Irfan Ahmad <31814510+Irfan4@users.noreply.github.com>
2025-02-10 21:42:05 -0700

    Update a01.js

--- a01.js
+++ a01.js
@@ -2,9 +2,9 @@
   Basic File I/O for displaying
   Skeleton Author: Joshua A. Levine
   Modified by: Amir Mohammad Esmaieeli Sikaroudi
-  Email: amesmaieeli@email.arizona.edu
+  Adapted for 433 HW1 Submission by: Irfan Ahmad
+  Email: irfana@arizona.edu
 */
-
 
 //access DOM elements we'll use
 var input = document.getElementById("load_image");
@@ -16,6 +16,70 @@ var width = 0;
 var height = 0;
 // The image data
 var ppm_img_data;
+
+let currentAngle = 0; // Current rotation angle
+
+/**
+ * Rotates an image perpendicular to the view axis counter-clockwise.
+ * 
+ * This function moves the image to the origin, rotates the image accordingly,
+ * and then moves it back to the original position.  These matrices create the
+ * transformation matrix, which is used to re-map all pixels of the image to their
+ * new rotated location.  A transformation matrix is created for each frame of the rotation,
+ * using the current angle.
+ * 
+ * @param - None
+ * @returns - None
+ */
+function rotate() {
+    // Create a new image data object to hold the new image
+    var newImageData = ctx.createImageData(width, height);
+
+    // Move the image to the origin
+    const movedToOrigin = GetTranslationMatrix(-width/2, -height/2);
+
+    // Rotate the image
+    const rotated = GetRotationMatrix(currentAngle);
+
+    // Return the image back to the original position
+    const returnedFromOrigin = GetTranslationMatrix(width/2, height/2);
+    
+    // Mix the translation and scale matrices
+    let transformationMatrix = MultiplyMatrixMatrix(returnedFromOrigin, MultiplyMatrixMatrix(rotated, movedToOrigin));
+    
+    // Loop through all the pixels in the image and set its color
+    for (var i = 0; i < ppm_img_data.data.length; i += 4) {
+
+        // Get the pixel location in x and y with (0,0) being the top left of the image
+        var pixel = [Math.floor(i/4) % width, 
+                     Math.floor(i/4 / width), 1];
+
+        // Get the location of the sample pixel
+        var samplePixel = MultiplyMatrixVector(transformationMatrix, pixel);
+
+        // Floor pixel to integer
+        samplePixel[0] = Math.round(samplePixel[0]);
+        samplePixel[1] = Math.round(samplePixel[1]);
+
+        // If the pixel is within the image bounds, only then move the pixel
+        if (samplePixel[0] >= 0 && samplePixel[0] < width && 
+            samplePixel[1] >= 0 && samplePixel[1] < height) {
+            setPixelColor(newImageData, samplePixel, i);
+        }
+    }
+
+    // Draw the new image
+    ctx.putImageData(newImageData, canvas.width/2 - width/2, canvas.height/2 - height/2);
+    
+    // Show the transformation matrix on HTML
+    showMatrix(transformationMatrix);
+    
+    // Update the angle for the next frame
+    currentAngle = (currentAngle + 1) % 360;
+    
+    // Keep rotating the image
+    requestAnimationFrame(rotate);
+}
 
 //Function to process upload
 var upload = function () {
@@ -31,43 +95,47 @@ var upload = function () {
             var file_data = fReader.result;
             parsePPM(file_data);
 
-            /*
-            * TODO: ADD CODE HERE TO DO 2D TRANSFORMATION and ANIMATION
-            * Modify any code if needed
-            * Hint: Write a rotation method, and call WebGL APIs to reuse the method for animation
-            */
-	    
-            // *** The code below is for the template to show you how to use matrices and update pixels on the canvas.
-            // *** Modify/remove the following code and implement animation
+            // Resize image to 600x600 if needed
+            if (width !== 600 || height !== 600) {
 
-	    // Create a new image data object to hold the new image
-            var newImageData = ctx.createImageData(width, height);
-	    var transMatrix = GetTranslationMatrix(0, height);// Translate image
-	    var scaleMatrix = GetScalingMatrix(1, -1);// Flip image y axis
-	    var matrix = MultiplyMatrixMatrix(transMatrix, scaleMatrix);// Mix the translation and scale matrices
-            
-            // Loop through all the pixels in the image and set its color
-            for (var i = 0; i < ppm_img_data.data.length; i += 4) {
+                // Create temporary canvas for resizing
+                var tempCanvas = document.createElement('canvas');
+                tempCanvas.width = 600;
+                tempCanvas.height = 600;
+                var tempCtx = tempCanvas.getContext('2d');
+                
+                // Create 600x600 image object
+                var tempImageData = tempCtx.createImageData(600, 600);
+                
+                var scaleX = width / 600;
+                var scaleY = height / 600;
+                
+                // Iterate through every pixel to rescale image
+                for (var y = 0; y < 600; y++) {
+                    for (var x = 0; x < 600; x++) {
+                        // Round down pixel index
+                        var sourceX = Math.floor(x * scaleX);
+                        var sourceY = Math.floor(y * scaleY);
 
-                // Get the pixel location in x and y with (0,0) being the top left of the image
-                var pixel = [Math.floor(i / 4) % width, 
-                             Math.floor(i / 4) / width, 1];
-        
-                // Get the location of the sample pixel
-                var samplePixel = MultiplyMatrixVector(matrix, pixel);
-
-                // Floor pixel to integer
-                samplePixel[0] = Math.floor(samplePixel[0]);
-                samplePixel[1] = Math.floor(samplePixel[1]);
-
-                setPixelColor(newImageData, samplePixel, i);
+                        var sourceIndex = (sourceY * width + sourceX) * 4;
+                        var targetIndex = (y * 600 + x) * 4;
+                        
+                        // Assign RGB values in the original image to the resized image
+                        tempImageData.data[targetIndex] = ppm_img_data.data[sourceIndex];
+                        tempImageData.data[targetIndex + 1] = ppm_img_data.data[sourceIndex + 1];
+                        tempImageData.data[targetIndex + 2] = ppm_img_data.data[sourceIndex + 2];
+                        tempImageData.data[targetIndex + 3] = 255;
+                    }
+                }
+                
+                width = 600;
+                height = 600;
+                ppm_img_data = tempImageData; // The actual image is now resized
             }
 
-            // Draw the new image
-            ctx.putImageData(newImageData, canvas.width/2 - width/2, canvas.height/2 - height/2);
-	    
-	    // Show matrix
-            showMatrix(matrix);
+            // Start the rotation animation now that the image is correctly sized
+            currentAngle = 0;
+            rotate();
         }
     }
 }
@@ -149,7 +217,6 @@ function parsePPM(file_data){
         image_data.data[i + 2] = bytes[pixel_pos * 3 + 2]; // Blue ~ i + 2
         image_data.data[i + 3] = 255; // A channel is deafult to 255
     }
-    ctx.putImageData(image_data, canvas.width/2 - width/2, canvas.height/2 - height/2);
     //ppm_img_data = ctx.getImageData(0, 0, canvas.width, canvas.height);   // This gives more than just the image I want??? I think it grabs white space from top left?
     ppm_img_data = image_data;
 }
